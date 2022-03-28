ed8a0fc0d191bf171f6714907c06aa5c132282b1
志村樹 <1shimutatsu.4646@gmail.com>
2022-03-28 14:05:05 +0000

    FIX：シードデータを追加。
    /db/seedsディレクトリ内の開発・本番環境用のシードファイルにコードを追加。

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -27,3 +27,63 @@ cities_list.each do |prefecture, city|
   prefecture = Prefecture.find_by(name: prefecture)
   prefecture.cities.create(name: city)
 end
+
+# トレーニーデータの作成
+5.times do |i|
+  Trainee.create!(
+    name: "trainee#{i}",
+    age: (20 + i),
+    gender: "male",
+    timeframe: nil,
+    introduction: nil,
+    category: nil,
+    instruction_method: nil,
+    dm_allowed: false,
+    email: "trainee#{i}@example.com",
+    password: "password#{i}"
+  )
+end
+
+# トレーナーデータの作成
+5.times do |i|
+  Trainer.create!(
+    name: "trainer#{i}",
+    age: (20 + i),
+    gender: "female",
+    timeframe: nil,
+    introduction: nil,
+    category: nil,
+    instruction_method: nil,
+    min_fee: nil,
+    max_fee: nil,
+    instruction_period: "unspecified",
+    email: "trainer#{i}@example.com",
+    password: "password#{i}"
+  )
+end
+
+# トレーニーのデフォルトのプロフィール画像のパス
+trainee_image_file = "default_trainee_avatar.png"
+trainee_image_path = Rails.root.join('app', 'assets', 'images', trainee_image_file)
+
+# トレーニーの活動地域
+trainee_city = City.where(id: [1]) # 札幌市
+
+# ユーザーの活動地域データの作成
+# ActiveStorageのavatarにデフォルトのプロフィール画像を添付
+Trainee.all.each do |trainee|
+  trainee.avatar.attach(io: File.open(trainee_image_path), filename: trainee_image_file)
+  trainee.cities << trainee_city
+end
+
+# トレーナーの活動地域
+trainer_city = City.where(id: [634]) # 千代田区
+
+# トレーナーのデフォルトのプロフィール画像のパス
+trainer_image_file = "default_trainer_avatar.png"
+trainer_image_path = Rails.root.join('app', 'assets', 'images', trainer_image_file)
+
+Trainer.all.each do |trainer|
+  trainer.avatar.attach(io: File.open(trainer_image_path), filename: trainer_image_file)
+  trainer.cities << trainer_city
+end
